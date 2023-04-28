ccd5a1dfc8405a16a7b23206513ae1ca72b695e8
Giovanni Palma <palma.giovanni@spes.uniud.it>
2023-04-28 15:08:49 +0200

    add comments

--- src/main.cpp
+++ src/main.cpp
@@ -8,23 +8,28 @@ std::string version = "1.0.0";
 int main(int argc, char *argv[]) {
     options.add_options()
             ("h,help", "Print usage")
-            ("s,share","Share file", cxxopts::value<std::string>())
-            ("p,public","Make it public and print the public URI")
-            ("version","Show software version");
+            ("s,share", "Share file", cxxopts::value<std::string>())
+            ("p,public", "Make it public and print the public URI")
+            ("version", "Show software version");
 
-    auto result = options.parse(argc,argv);
+    auto result = options.parse(argc, argv);
 
-    if(result.count("help")){
+    if (result.count("help")) {
         std::cout << options.help() << std::endl;
     }
 
-    if(result.count("share")){
+    if (result.count("share")) {
+        // Make unique id
+        // Open HTTP server
+        // Allow connection to <domain>/v/$id
+        // Stream media
+        // feature: make a way to share commands and sync clients
         std::string path = result["share"].as<std::string>();
         std::cout << "Share command executed" << std::endl;
     }
 
-    if(result.count("version")){
-        std::cout << "version: "<< version << std::endl;
+    if (result.count("version")) {
+        std::cout << "version: " << version << std::endl;
     }
     return 0;
 }

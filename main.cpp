ab1872455e1632fb9c734c0f09701acaaf2447e0
Giovanni Palma <palma.giovanni@spes.uniud.it>
2023-04-28 10:17:18 +0200

    cxxopts library integration

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <cxxopts.hpp>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+cxxopts::Options options("FastMediaShare", "Simple Media Share System");
+
+std::basic_string<char> version = "1.0.0";
+
+int main(int argc, char *argv[]) {
+    options.add_options()
+            ("version","Show software version")
+            ("s,share","Share file", cxxopts::value<std::string>())
+            ("p,public","Make it public, print the public URI");
+
+    auto result = options.parse(argc,argv);
+
+    if(result.count("share")){
+        std::cout << "Share command executed" << std::endl;
+    }
+
+    if(result.count("version")){
+        std::cout << "version: "<< version << std::endl;
+    }
+
     return 0;
 }

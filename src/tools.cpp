80884afa5a6cabb43f1914955b746596cf912e6c
Giovanni Palma <palma.giovanni@spes.uniud.it>
2023-04-28 15:07:46 +0200

    static functions

---
+++ src/tools.cpp
@@ -0,0 +1,38 @@
+//
+// Created by giovanni on 28/04/23.
+//
+
+#include "tools.h"
+
+std::string tools::random_string(std::size_t length) {
+    const std::string CHARACTERS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    std::random_device random_device;
+    std::mt19937 generator(random_device());
+    std::uniform_int_distribution<> distribution(0, CHARACTERS.size() - 1);
+
+    std::string random_string;
+
+    std::generate_n(std::back_inserter(random_string), length, [&] { return CHARACTERS[distribution(generator)]; });
+
+    return random_string;
+}
+
+size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
+    ((std::string *) userp)->append((char *) contents, size * nmemb);
+    return size * nmemb;
+}
+
+std::string tools::getPublicIp() {
+    CURL *curl;
+    std::string readBuffer;
+
+    curl = curl_easy_init();
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "https://api.ipify.org");
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+        curl_easy_cleanup(curl);
+    }
+    return readBuffer;
+}
---
+++ src/tools.h
@@ -0,0 +1,21 @@
+//
+// Created by giovanni on 28/04/23.
+//
+
+#ifndef FASTMEDIASHARE_TOOLS_H
+#define FASTMEDIASHARE_TOOLS_H
+
+#include <string>
+#include <random>
+#include <algorithm>
+#include <curl/curl.h>
+
+class tools {
+public:
+    static std::string random_string(std::size_t length);
+
+    static std::string getPublicIp();
+};
+
+
+#endif //FASTMEDIASHARE_TOOLS_H

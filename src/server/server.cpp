69b94373578e7a541c6c2d32a553ff1565b9576d
Giovanni Palma <palma.giovanni@spes.uniud.it>
2023-04-29 12:10:06 +0200

    initialize oatpp server

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,18 +1,16 @@
-//
-// Created by giovanni on 28/04/23.
-//
-
 #include <unordered_map>
 #include "server.h"
 #include "../tools.h"
+#include <sstream>
 
 std::unordered_map<std::string, std::string> map;
+const size_t kIdLen = 10;
 
-std::string server::GenerateUri(const std::string &path, bool remote_access = false) {
+std::string server::GenerateUri(const std::string &path, const bool remote_access = false) {
 
   std::string id = "";
   do {
-    id = tools::RandomString(5);
+    id = tools::RandomString(kIdLen);
   } while (map.find(id) != map.end()); // This while check if the generated id already exists
 
   map.insert(std::make_pair(id, path));
@@ -20,5 +18,24 @@ std::string server::GenerateUri(const std::string &path, bool remote_access = fa
   if (remote_access) {
     return tools::GetPublicIp() + "/v/" + id;
   }
-  return "localhost/v/" + id;;
+  return "127.0.0.1/v/" + id;;
+}
+void server::Run(const std::string host, const unsigned short port) {
+  auto router = oatpp::web::server::HttpRouter::createShared();
+  auto connection_handler = oatpp::web::server::HttpConnectionHandler::createShared(router);
+  auto connection_provider =
+      oatpp::network::tcp::server::ConnectionProvider::createShared({"localhost", port, oatpp::network::Address::IP_4});
+
+  oatpp::network::Server server(connection_provider, connection_handler);
+  OATPP_LOGI("FMS",
+             "Server running on %s:%s",
+             host.c_str(),
+             std::to_string(port).c_str());
+  server.run();
+}
+void server::init() {
+  oatpp::base::Environment::init();
+}
+void server::destroy() {
+  oatpp::base::Environment::destroy();
 }
--- src/server/server.h
+++ src/server/server.h
@@ -5,7 +5,10 @@
 #ifndef FASTMEDIASHARE_SERVER_H
 #define FASTMEDIASHARE_SERVER_H
 
-#include "string"
+#include <iostream>
+#include "oatpp/web/server/HttpConnectionHandler.hpp"
+#include "oatpp/network/Server.hpp"
+#include "oatpp/network/tcp/server/ConnectionProvider.hpp"
 
 class server {
  public:
@@ -16,6 +19,9 @@ class server {
  * @return url of media file
  */
   static std::string GenerateUri(const std::string &path, bool remote_access);
+  static void Run(const std::string host, v_uint16 port);
+  void static init();
+  void static destroy();
 };
 
 #endif //FASTMEDIASHARE_SERVER_H
